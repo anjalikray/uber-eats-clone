aa1fe9973466186afca7036862de6828cda87ae2
anjalikray <anjalikumari2907@gmail.com>
2025-12-02 16:14:30 +0530

    first commit - configiring lerna

---
+++ packages/uber-eats/__tests__/uber-eats.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const uberEats = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(uberEats(), 'Hello from uberEats');
+console.info('uberEats tests passed');
---
+++ packages/uber-eats/lib/uber-eats.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = uberEats;
+
+function uberEats() {
+  return 'Hello from uberEats';
+}
