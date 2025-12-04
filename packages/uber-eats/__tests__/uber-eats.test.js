3251fc73d5939b1e151a3236ab0e3408e9b29801
anjalikray <anjalikumari2907@gmail.com>
2025-12-04 18:52:04 +0530

    configured project

--- packages/uber-eats/__tests__/uber-eats.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const uberEats = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(uberEats(), 'Hello from uberEats');
-console.info('uberEats tests passed');
--- packages/uber-eats/lib/uber-eats.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = uberEats;
-
-function uberEats() {
-  return 'Hello from uberEats';
-}
